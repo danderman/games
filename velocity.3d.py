90b0a08f546abb5728f99c953f0099a2c118debb
Daryl Anderman <daryl@273kelvin.ca>
2018-08-15 17:03:21 -0400

    add loss condition, constrain x too, add guides

--- velocity.3d.py
+++ velocity.3d.py
@@ -3,14 +3,15 @@ from math import sqrt
 import pyglet as p
 p.options['debug_gl'] = False
 from pyglet.window import key
-screen = p.window.Window(900, 900)
+screen = p.window.Window(850, 850)
 keyboard = key.KeyStateHandler()
 screen.push_handlers(keyboard)
 screen.set_exclusive_mouse(True)
 
 def update(dt):
-	global lastgate, bufpos, level, levelcnt, points
+	global lastgate, bufpos, level, levelcnt, points, gatesmissed
 	if keyboard[key.Q]: sys.exit('You got %s points' % (points,))
+	if gatesmissed > maxgatesmissed: sys.exit('You got %s points' % (points,))
 	for g in pos:
 		g.x-=2
 	for g in v:
@@ -18,17 +19,27 @@ def update(dt):
 	for g in a:
 		g.x-=2
 	for g in gates:
-		g.scale -= scaledelta
+		g.scale *= spritescaledelta
+		xdiff = (g.right - g.left)
+		ydiff = (g.top - g.bottom)
+		g.top -= ydiff * scaledelta
+		g.bottom += ydiff * scaledelta
+		g.right -= xdiff * scaledelta
+		g.left += xdiff * scaledelta
+		g.x = g.left + g.x_percent * xdiff
+		g.y = g.bottom + g.y_percent * ydiff
 		g.opacity = constrain(opacitydelta+g.opacity, 0, 255)
 		if g.scale < 1 and g.image is sbgate:
-			if abs(me.y - g.y) < (g.height/2 - radius):
-				if abs(me.x - g.x) < (g.height/2 - radius):
+			if me.y - radius > g.y and me.y + radius < g.y + g.height:
+				if me.x - radius > g.x and me.x + radius < g.x + g.width:
 					g.image=sggate
 					points += 1
 				else:
 					g.image=srgate
+					gatesmissed += 1
 			else:
 				g.image=srgate
+				gatesmissed += 1
 
 	bufnext = bufpos + 1
 	bufnext = bufnext % bufsize
@@ -60,20 +71,26 @@ def update(dt):
 		me.y = y_abuf[bufpos]
 
 
-	me.y = constrain(me.y, radius, height-radius)
+	me.x = constrain(me.x, left+radius, right-radius)
+	me.y = constrain(me.y, bottom+radius, top-radius)
 
 	bufpos = bufnext
 
 	if random.random()*3 + lastgate > 4:
-		gates.append(p.sprite.Sprite(img=sbgate, x=int(random.randint(sbgate.width//2, width-sbgate.width//2)),y=int(random.randint(sbgate.height//2, height-sbgate.height//2))))
-		gates[-1].scale = 3
-		gates[-1].opacity = 64
+		gate = p.sprite.Sprite(img=sbgate, x=int(random.randint(1, width-(sbgate.width*3))),y=int(random.randint(1, height-(sbgate.height*3))))
+		#gate = p.sprite.Sprite(img=sbgate, x=1,y=width-(sbgate.width*3))
+		gate.scale = 3
+		gate.opacity = 64
+		gate.x_percent = (gate.x/width)
+		gate.y_percent = (gate.y/height)
+		gate.left, gate.right, gate.top, gate.bottom=1,width-1,height-1,1
+		gates.append(gate)
 		lastgate = 0
 		levelcnt += 1
 	else:
 		lastgate += dt
 	for g in range(len(gates)-1,-1,-1):
-		if gates[g].scale < 0:
+		if gates[g].scale < 0.5:
 			del(gates[g])
 	for g in range(len(pos)-1,0,-1):
 		if pos[g].x < 0:
@@ -86,22 +103,36 @@ def update(dt):
 			del(a[g])
 	if levelcnt == 20:
 		level = 2
-	elif levelcnt == 40: 
+	elif levelcnt == 40:
 		level = 3
-	status.text = 'Level: %s, Points: %s, Pos: %s' % (level, points, me.y)
+	status.text = 'Level: %s, Points: %s, Pos: %s, Missed: %s' % (level, points, me.y, gatesmissed)
 
 @screen.event
 def on_draw():
-	p.gl.glPolygonMode(p.gl.GL_FRONT_AND_BACK, p.gl.GL_FILL);
 	screen.clear()
-	me.draw()
 	for g in gates:
-		g.draw()
+		p.graphics.draw(4, p.gl.GL_POLYGON,
+    		('v2f', (g.left, g.top, g.right, g.top, g.right, g.bottom, g.left, g.bottom)))
+	p.graphics.draw(16, p.gl.GL_LINES,
+    	('v2f', (0,0, ileft, ibottom,
+    		0, height, ileft, itop,
+    		width, height, iright, itop,
+    		width, 0, iright, ibottom,
+    		0, center_y, ileft, center_y,
+    		center_x, height, center_x, itop,
+    		width, center_y, iright, center_y,
+    		center_x, 0, center_x, ibottom)))
+	p.gl.glPolygonMode(p.gl.GL_FRONT_AND_BACK, p.gl.GL_FILL);
 	lines.draw()
+	for g in [x for x in gates if x.scale < 1]:
+		g.draw()
+	me.draw()
+	for g in [x for x in gates if x.scale >= 1]:
+		g.draw()
 	status.draw()
 	p.gl.glPolygonMode(p.gl.GL_FRONT_AND_BACK, p.gl.GL_LINE);
 	p.graphics.draw(4, p.gl.GL_POLYGON,
-    ('v2i', (upper_left_x,upper_left_y, upper_right_x, upper_right_y, lower_right_x,lower_right_y, lower_left_x, lower_left_y)))
+    	('v2f', (left,top, right, top, right, bottom, left, bottom)))
 
 
 def center_image(image):
@@ -115,29 +146,25 @@ def on_mouse_motion(x, y, dx, dy):
 	mousey_pos += dy
 	mousex_pos += dx
 
-p.resource.path = ['/Users/notme/devel/games/res']
+p.resource.path = ['res']
 p.resource.reindex()
 circ = p.resource.image('circ.png')
 center_image(circ)
-ggate = p.resource.image('ggate.png')
-center_image(ggate)
-rgate = p.resource.image('rgate.png')
-center_image(rgate)
-bgate = p.resource.image('bgate.png')
-center_image(bgate)
 sggate = p.resource.image('sggate.png')
-center_image(sggate)
+#center_image(sggate)
 srgate = p.resource.image('srgate.png')
-center_image(srgate)
+#center_image(srgate)
 sbgate = p.resource.image('sbgate.png')
-center_image(sbgate)
+#center_image(sbgate)
 
 pl = p.resource.image('pl.png')
 vl = p.resource.image('vl.png')
 al = p.resource.image('al.png')
 
 size = width, height = screen.width, screen.height
-scaledelta = width / sggate.width / 500
+center_x, center_y = width//2, height//2
+spritescaledelta = 1-(width / sggate.width / 1000) #This constant needs to change in inverse relation
+scaledelta = spritescaledelta * .00421 # to this one
 opacitydelta = 2
 radius = 10
 mousey_pos = height//2
@@ -145,6 +172,8 @@ mousex_pos = width//2
 level = 1
 levelcnt = 0
 points = 0
+gatesmissed = 0
+maxgatesmissed = 5
 lastgate = 0
 bufsize = 10
 x_pbuf = [height/2] * bufsize
@@ -159,15 +188,14 @@ y_vdbuf = [0] * bufsize
 y_abuf = [0] * bufsize
 bufpos = 0
 status = p.text.Label('', x=width//2, y=radius, anchor_x='center')
-upper_left_x = 50
-upper_left_y = 950
-upper_right_x = 950
-upper_right_y = 950
-lower_left_x = 50
-lower_left_y = 50
-lower_right_x = 950
-lower_right_y = 50
-
+left = width/850*283
+ileft = left+sggate.width/1.3
+right = left + width/850*288
+iright = right-sggate.width/1.3
+bottom = width/850*283
+ibottom = bottom+sggate.width/1.3
+top = bottom + width/850*288
+itop = top-sggate.width/1.3
 def constrain(val, minv, maxv):
 	if val < minv:
 		return minv

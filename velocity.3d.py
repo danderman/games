7bd2fe4c9176f9ae82d4e092d773fe30cd407e69
Daryl Anderman <daryl@273kelvin.ca>
2018-08-16 14:47:54 -0400

    stop using global variables, add start mode

--- velocity.3d.py
+++ velocity.3d.py
@@ -9,194 +9,151 @@ screen.push_handlers(keyboard)
 screen.set_exclusive_mouse(True)
 
 def update(dt):
-	global lastgate, bufpos, level, levelcnt, points, gatesmissed
-	if keyboard[key.Q]: sys.exit('You got %s points' % (points,))
-	if gatesmissed > maxgatesmissed: sys.exit('You got %s points' % (points,))
-	for g in pos:
-		g.x-=2
-	for g in v:
-		g.x-=2
-	for g in a:
-		g.x-=2
-	for g in gates:
-		g.scale *= spritescaledelta
-		xdiff = (g.right - g.left)
-		ydiff = (g.top - g.bottom)
-		g.top -= ydiff * scaledelta
-		g.bottom += ydiff * scaledelta
-		g.right -= xdiff * scaledelta
-		g.left += xdiff * scaledelta
-		g.x = g.left + g.x_percent * xdiff
-		g.y = g.bottom + g.y_percent * ydiff
-		g.opacity = constrain(opacitydelta+g.opacity, 0, 255)
-		if g.scale < 1 and g.image is sbgate:
-			if me.y - radius > g.y and me.y + radius < g.y + g.height:
-				if me.x - radius > g.x and me.x + radius < g.x + g.width:
-					g.image=sggate
-					points += 1
+	global g
+	if keyboard[key.Q]: sys.exit('You got %s points' % (g.points,))
+	if g.mode == 'start':
+		if keyboard[key.SPACE]:
+			g.mode = 'play'
+		return
+	if g.gatesmissed > g.maxgatesmissed: g = Game(g.points)
+	for l in g.pos:
+		l.x-=2
+	for l in g.v:
+		l.x-=2
+	for l in g.a:
+		l.x-=2
+	for gt in g.gates:
+		gt.scale *= g.spritescaledelta
+		xdiff = (gt.right - gt.left)
+		ydiff = (gt.top - gt.bottom)
+		gt.top -= ydiff * g.scaledelta
+		gt.bottom += ydiff * g.scaledelta
+		gt.right -= xdiff * g.scaledelta
+		gt.left += xdiff * g.scaledelta
+		gt.x = gt.left + gt.x_percent * xdiff
+		gt.y = gt.bottom + gt.y_percent * ydiff
+		gt.opacity = constrain(g.opacitydelta+gt.opacity, 0, 255)
+		if gt.scale < 1 and gt.image is g.sbgate:
+			if g.me.y - g.radius > gt.y and g.me.y + g.radius < gt.y + gt.height:
+				if g.me.x - g.radius > gt.x and g.me.x + g.radius < gt.x + gt.width:
+					gt.image=g.sggate
+					g.points += 1
 				else:
-					g.image=srgate
-					gatesmissed += 1
+					gt.image=g.srgate
+					g.gatesmissed += 1
 			else:
-				g.image=srgate
-				gatesmissed += 1
+				gt.image=g.srgate
+				g.gatesmissed += 1
 
+	bufpos = g.bufpos
 	bufnext = bufpos + 1
-	bufnext = bufnext % bufsize
+	bufnext = bufnext % g.bufsize
 	bufprev = bufpos - 1
 	if bufprev == -1:
-		bufprev = bufsize -1
+		bufprev = g.bufsize -1
 
-	x_pbuf[bufpos] = mousex_pos
-	y_pbuf[bufpos] = mousey_pos
-	pos.append(p.sprite.Sprite(img=pl, x=width//2-radius, y=y_pbuf[bufpos], batch=lines))
-	x_pdbuf[bufpos] = mousex_pos - x_pbuf[bufprev]
-	y_pdbuf[bufpos] = mousey_pos - y_pbuf[bufprev]
-	x_vbuf[bufpos] = sum(x_pdbuf) / bufsize + height / 2
-	y_vbuf[bufpos] = sum(y_pdbuf) / bufsize + height / 2
-	v.append(p.sprite.Sprite(img=vl, x=width//2-radius, y=y_vbuf[bufpos], batch=lines))
-	x_vdbuf[bufpos] = 3 * (x_vbuf[bufpos] - x_vbuf[bufprev]) # straight acceleration isn't responsive enough
-	y_vdbuf[bufpos] = 3 * (y_vbuf[bufpos] - y_vbuf[bufprev]) # straight acceleration isn't responsive enough
-	x_abuf[bufpos] = sum(x_vdbuf) / bufsize + height / 2
-	y_abuf[bufpos] = sum(y_vdbuf) / bufsize + height / 2
-	a.append(p.sprite.Sprite(img=al, x=width//2-radius, y=y_abuf[bufpos], batch=lines))
-	if level == 1:
-		me.x = mousex_pos
-		me.y = mousey_pos
-	elif level == 2:
-		me.x = x_vbuf[bufpos]
-		me.y = y_vbuf[bufpos]
-	elif level == 3:
-		me.x = x_abuf[bufpos]
-		me.y = y_abuf[bufpos]
+	g.x_pbuf[bufpos] = g.mousex_pos
+	g.y_pbuf[bufpos] = g.mousey_pos
+	g.pos.append(p.sprite.Sprite(img=g.pl, x=g.width//2-g.radius, y=g.y_pbuf[bufpos], batch=g.lines))
+	g.x_pdbuf[bufpos] = g.mousex_pos - g.x_pbuf[bufprev]
+	g.y_pdbuf[bufpos] = g.mousey_pos - g.y_pbuf[bufprev]
+	g.x_vbuf[bufpos] = sum(g.x_pdbuf) / g.bufsize + g.center_x
+	g.y_vbuf[bufpos] = sum(g.y_pdbuf) / g.bufsize + g.center_y
+	g.v.append(p.sprite.Sprite(img=g.vl, x=g.width//2-g.radius, y=g.y_vbuf[bufpos], batch=g.lines))
+	g.x_vdbuf[bufpos] = 4 * (g.x_vbuf[bufpos] - g.x_vbuf[bufprev]) # straight acceleration isn't responsive enough
+	g.y_vdbuf[bufpos] = 4 * (g.y_vbuf[bufpos] - g.y_vbuf[bufprev]) # straight acceleration isn't responsive enough
+	g.x_abuf[bufpos] = sum(g.x_vdbuf) / g.bufsize + g.height / 2
+	g.y_abuf[bufpos] = sum(g.y_vdbuf) / g.bufsize + g.height / 2
+	g.a.append(p.sprite.Sprite(img=g.al, x=g.width//2-g.radius, y=g.y_abuf[bufpos], batch=g.lines))
+	if g.level == 1:
+		g.me.x = g.mousex_pos
+		g.me.y = g.mousey_pos
+	elif g.level == 2:
+		g.me.x = g.x_vbuf[bufpos]
+		g.me.y = g.y_vbuf[bufpos]
+	elif g.level == 3:
+		g.me.x = g.x_abuf[bufpos]
+		g.me.y = g.y_abuf[bufpos]
 
 
-	me.x = constrain(me.x, left+radius, right-radius)
-	me.y = constrain(me.y, bottom+radius, top-radius)
+	g.me.x = constrain(g.me.x, g.left+g.radius, g.right-g.radius)
+	g.me.y = constrain(g.me.y, g.bottom+g.radius, g.top-g.radius)
 
-	bufpos = bufnext
+	g.bufpos = bufnext
 
-	if random.random()*3 + lastgate > 4:
-		gate = p.sprite.Sprite(img=sbgate, x=int(random.randint(1, width-(sbgate.width*3))),y=int(random.randint(1, height-(sbgate.height*3))))
+	if random.random()*3 + g.lastgate > 4.5:
+		gate = p.sprite.Sprite(img=g.sbgate, x=int(random.randint(1, g.width-(g.sbgate.width*3))),y=int(random.randint(1, g.height-(g.sbgate.height*3))))
 		#gate = p.sprite.Sprite(img=sbgate, x=1,y=width-(sbgate.width*3))
 		gate.scale = 3
-		gate.opacity = 64
-		gate.x_percent = (gate.x/width)
-		gate.y_percent = (gate.y/height)
-		gate.left, gate.right, gate.top, gate.bottom=1,width-1,height-1,1
-		gates.append(gate)
-		lastgate = 0
-		levelcnt += 1
+		gate.opacity = 32
+		gate.x_percent = (gate.x/g.width)
+		gate.y_percent = (gate.y/g.height)
+		gate.left, gate.right, gate.top, gate.bottom=1,g.width-1,g.height-1,1
+		g.gates.append(gate)
+		g.lastgate = 0
+		g.levelcnt += 1
 	else:
-		lastgate += dt
-	for g in range(len(gates)-1,-1,-1):
-		if gates[g].scale < 0.5:
-			del(gates[g])
-	for g in range(len(pos)-1,0,-1):
-		if pos[g].x < 0:
-			del(pos[g])
-	for g in range(len(v)-1,0,-1):
-		if v[g].x < 0:
-			del(v[g])
-	for g in range(len(a)-1,0,-1):
-		if a[g].x < 0:
-			del(a[g])
-	if levelcnt == 20:
-		level = 2
-	elif levelcnt == 40:
-		level = 3
-	status.text = 'Level: %s, Points: %s, Pos: %s, Missed: %s' % (level, points, me.y, gatesmissed)
+		g.lastgate += dt
+	for gt in range(len(g.gates)-1,-1,-1):
+		if g.gates[gt].scale < 0.5:
+			del(g.gates[gt])
+	for i in range(len(g.pos)-1,0,-1):
+		if g.pos[i].x < 0:
+			del(g.pos[i])
+	for i in range(len(g.v)-1,0,-1):
+		if g.v[i].x < 0:
+			del(g.v[i])
+	for i in range(len(g.a)-1,0,-1):
+		if g.a[i].x < 0:
+			del(g.a[i])
+	if g.levelcnt == 20:
+		g.level = 2
+	elif g.levelcnt == 40:
+		g.level = 3
+	g.status.text = 'Level: %s, Points: %s, Pos: %s, Missed: %s' % (g.level, g.points, g.me.y, g.gatesmissed)
 
 @screen.event
 def on_draw():
 	screen.clear()
-	for g in gates:
+	if g.mode == 'start':
+		p.gl.glPolygonMode(p.gl.GL_FRONT_AND_BACK, p.gl.GL_FILL);
+		g.startmsg.draw()
+	elif g.mode == 'play':
+		for gt in g.gates:
+			p.graphics.draw(4, p.gl.GL_POLYGON,
+				('v2f', (gt.left, gt.top, gt.right, gt.top, gt.right, gt.bottom, gt.left, gt.bottom)))
+		p.graphics.draw(16, p.gl.GL_LINES,
+			('v2f', (0,0, g.ileft, g.ibottom,
+				0, g.height, g.ileft, g.itop,
+				g.width, g.height, g.iright, g.itop,
+				g.width, 0, g.iright, g.ibottom,
+				0, g.center_y, g.ileft, g.center_y,
+				g.center_x, g.height, g.center_x, g.itop,
+				g.width, g.center_y, g.iright, g.center_y,
+				g.center_x, 0, g.center_x, g.ibottom)))
+		p.gl.glPolygonMode(p.gl.GL_FRONT_AND_BACK, p.gl.GL_FILL);
+		g.lines.draw()
+		for gt in [x for x in g.gates if x.scale < 1]:
+			gt.draw()
+		g.me.draw()
+		for gt in [x for x in g.gates if x.scale >= 1]:
+			gt.draw()
+		g.status.draw()
+		p.gl.glPolygonMode(p.gl.GL_FRONT_AND_BACK, p.gl.GL_LINE);
 		p.graphics.draw(4, p.gl.GL_POLYGON,
-    		('v2f', (g.left, g.top, g.right, g.top, g.right, g.bottom, g.left, g.bottom)))
-	p.graphics.draw(16, p.gl.GL_LINES,
-    	('v2f', (0,0, ileft, ibottom,
-    		0, height, ileft, itop,
-    		width, height, iright, itop,
-    		width, 0, iright, ibottom,
-    		0, center_y, ileft, center_y,
-    		center_x, height, center_x, itop,
-    		width, center_y, iright, center_y,
-    		center_x, 0, center_x, ibottom)))
-	p.gl.glPolygonMode(p.gl.GL_FRONT_AND_BACK, p.gl.GL_FILL);
-	lines.draw()
-	for g in [x for x in gates if x.scale < 1]:
-		g.draw()
-	me.draw()
-	for g in [x for x in gates if x.scale >= 1]:
-		g.draw()
-	status.draw()
-	p.gl.glPolygonMode(p.gl.GL_FRONT_AND_BACK, p.gl.GL_LINE);
-	p.graphics.draw(4, p.gl.GL_POLYGON,
-    	('v2f', (left,top, right, top, right, bottom, left, bottom)),
-    	('c3B', (254, 254, 0, 254, 254, 0, 254, 254, 0, 254, 254, 0)))
-
-
-def center_image(image):
-    """Sets an image's anchor point to its center"""
-    image.anchor_x = image.width // 2
-    image.anchor_y = image.height // 2
+			('v2f', (g.left, g.top, g.right, g.top, g.right, g.bottom, g.left, g.bottom)),
+			('c3B', (254, 254, 0, 254, 254, 0, 254, 254, 0, 254, 254, 0)))
 
 @screen.event
 def on_mouse_motion(x, y, dx, dy):
 	global mousey_pos, mousex_pos
-	mousey_pos += dy
-	mousex_pos += dx
+	g.mousey_pos += dy
+	g.mousex_pos += dx
 
-p.resource.path = ['res']
-p.resource.reindex()
-circ = p.resource.image('circ.png')
-center_image(circ)
-sggate = p.resource.image('sggate.png')
-#center_image(sggate)
-srgate = p.resource.image('srgate.png')
-#center_image(srgate)
-sbgate = p.resource.image('sbgate.png')
-#center_image(sbgate)
+@screen.event
+def on_mouse_press(x, y, btn, mods):
+	g.mode = 'play'
 
-pl = p.resource.image('pl.png')
-vl = p.resource.image('vl.png')
-al = p.resource.image('al.png')
-
-size = width, height = screen.width, screen.height
-center_x, center_y = width//2, height//2
-spritescaledelta = 1-(width / sggate.width / 1000) #This constant needs to change in inverse relation
-scaledelta = spritescaledelta * .00421 # to this one
-opacitydelta = 2
-radius = 10
-mousey_pos = height//2
-mousex_pos = width//2
-level = 1
-levelcnt = 0
-points = 0
-gatesmissed = 0
-maxgatesmissed = 5
-lastgate = 0
-bufsize = 10
-x_pbuf = [height/2] * bufsize
-x_pdbuf = [0] * bufsize
-x_vbuf = [0] * bufsize
-x_vdbuf = [0] * bufsize
-x_abuf = [0] * bufsize
-y_pbuf = [height/2] * bufsize
-y_pdbuf = [0] * bufsize
-y_vbuf = [0] * bufsize
-y_vdbuf = [0] * bufsize
-y_abuf = [0] * bufsize
-bufpos = 0
-status = p.text.Label('', x=width//2, y=radius, anchor_x='center')
-left = width/850*283
-ileft = left+sggate.width/1.3
-right = left + width/850*288
-iright = right-sggate.width/1.3
-bottom = width/850*283
-ibottom = bottom+sggate.width/1.3
-top = bottom + width/850*288
-itop = top-sggate.width/1.3
 def constrain(val, minv, maxv):
 	if val < minv:
 		return minv
@@ -205,14 +162,78 @@ def constrain(val, minv, maxv):
 	else:
 		return val
 
-me = p.sprite.Sprite(img=circ, x=width/2,y=height/2)
-gates = []
-a = []
-v = []
-pos = []
-lines = p.graphics.Batch()
+class Game():
+	"""docstring for Game"""
+	def __init__(self, prevpoints):
+		p.resource.path = ['res']
+		p.resource.reindex()
+		self.mode = 'start'
+		self.circ = p.resource.image('circ.png')
+		self.center_image(self.circ)
+		self.sggate = p.resource.image('sggate.png')
+		#center_image(sggate)
+		self.srgate = p.resource.image('srgate.png')
+		#center_image(srgate)
+		self.sbgate = p.resource.image('sbgate.png')
+		#center_image(sbgate)
+
+		self.pl = p.resource.image('pl.png')
+		self.vl = p.resource.image('vl.png')
+		self.al = p.resource.image('al.png')
+
+		self.width, self.height = screen.width, screen.height
+		self.center_x, self.center_y = self.width//2, self.height//2
+		self.startmsg = p.text.Label('Click or press space to start.\nPrevious game points: %s' % (prevpoints,), x=self.center_x, y=self.center_y, anchor_x='center')
+		self.spritescaledelta = 1-(self.width / self.sggate.width / 1000) #This constant needs to change in inverse relation
+		self.scaledelta = self.spritescaledelta * .00421 # to this one
+		self.opacitydelta = 2
+		self.radius = 10
+		self.mousey_pos = self.center_y
+		self.mousex_pos = self.center_x
+		self.level = 1
+		self.levelcnt = 0
+		self.points = 0
+		self.gatesmissed = 0
+		self.maxgatesmissed = 5
+		self.lastgate = 0
+		self.bufsize = bufsize = 10
+		self.x_pbuf = [self.height/2] * bufsize
+		self.x_pdbuf = [0] * bufsize
+		self.x_vbuf = [0] * bufsize
+		self.x_vdbuf = [0] * bufsize
+		self.x_abuf = [0] * bufsize
+		self.y_pbuf = [self.height/2] * bufsize
+		self.y_pdbuf = [0] * bufsize
+		self.y_vbuf = [0] * bufsize
+		self.y_vdbuf = [0] * bufsize
+		self.y_abuf = [0] * bufsize
+		self.bufpos = 0
+		self.status = p.text.Label('', x=self.width//2, y=self.radius, anchor_x='center')
+		self.left = self.width/850*283
+		self.ileft = self.left+self.sggate.width/1.3
+		self.right = self.left + self.width/850*288
+		self.iright = self.right-self.sggate.width/1.3
+		self.bottom = self.width/850*283
+		self.ibottom = self.bottom+self.sggate.width/1.3
+		self.top = self.bottom + self.width/850*288
+		self.itop = self.top-self.sggate.width/1.3
+		self.me = p.sprite.Sprite(img=self.circ, x=self.width/2,y=self.height/2)
+		self.gates = []
+		self.a = []
+		self.v = []
+		self.pos = []
+		self.lines = p.graphics.Batch()
+
+	def center_image(self,image):
+		"""Sets an image's anchor point to its center"""
+		image.anchor_x = image.width // 2
+		image.anchor_y = image.height // 2
+
+
+
 
 
 if __name__ == '__main__':
+	g = Game(0)
 	p.clock.schedule_interval(update, 1/60.0)
 	p.app.run()
